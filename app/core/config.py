b80ed24dde4293eb245331ae151112f77ce4515b
Marcelo Bezerra <marcelojuniorbzerra12@gmail.com>
2025-05-26 14:39:06 -0300

    Adicionado verificacao no config

--- app/core/config.py
+++ app/core/config.py
@@ -5,3 +5,8 @@ load_dotenv()
 
 PIPEFY_TOKEN = os.getenv("PIPEFY_TOKEN")
 PIPEFY_API_URL = os.getenv("PIPEFY_API_URL")
+
+if not PIPEFY_TOKEN or not PIPEFY_API_URL:
+    raise ValueError(
+        "As variaveis de ambiente PIPEFY_TOKEN e PIPEFY_API_URL devem ser definidas."
+    )
